f3f82c12b3358a38456985c44630807f9761c239
constantinmirza <pesbogdan@yahoo.com>
2017-01-07 01:21:12 +0200

    Am declarat principalele variabile si am creat functia pentru a seta valorile de start.

--- proiect/main.cpp
+++ proiect/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <stdlib.h>
 using namespace std;
-
+bool ConditieJoc;
+const int latime=20;
+const int inaltime=20;
+int x,y,scor,CoordX,CoordY;
+enum Directii {stop=0, LEFT,RIGHT,UP,DOWN};
+Directii direc;
+void Setari()
+{
+    ConditieJoc=0;
+    direc=stop;
+    x=latime/2;
+    y=inaltime/2;
+    CoordX=rand()%latime;
+    CoordY=rand()%inaltime;
+    scor=0;
+}
 int main()
 {
-
     return 0;
 }
