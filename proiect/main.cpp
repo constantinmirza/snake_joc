ae1623f9b3a20555822c34f4126e09628e206f7d
constantinmirza <pesbogdan@yahoo.com>
2017-01-08 03:12:24 +0200

    Am facut marginile jocului

--- proiect/main.cpp
+++ proiect/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 using namespace std;
 bool ConditieJoc;
-const int latime=20;
+const int latime=40;
 const int inaltime=20;
 int x,y,scor,CoordX,CoordY;
 enum Directii {stop=0, LEFT,RIGHT,UP,DOWN};
@@ -18,7 +19,36 @@ void Setari()
     CoordY=rand()%inaltime;
     scor=0;
 }
+void Ecran()
+{
+    system("cls");
+    int i,j;
+    for(i=0; i<latime+2; i++)
+        cout<<"#";
+    cout<<endl;
+    for(i=0; i<inaltime; i++)
+    {
+        for(j=0; j<latime; j++)
+        {
+            if(j==0)
+                cout<<"#";
+            cout<<" ";
+            if(j==latime-1)
+                cout<<"#";
+        }
+        cout<<endl;
+    }
+    for(i=0; i<latime+2; i++)
+        cout<<"#";
+    cout<<endl;
+}
 int main()
 {
+    Setari();
+    while(!ConditieJoc)
+   {
+       Ecran();
+       Sleep(100);
+   }
     return 0;
 }
